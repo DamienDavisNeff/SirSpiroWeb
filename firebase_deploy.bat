7504c4e80fcbf5c6e0cb3780dfc76fa89803b2b4
Damien Davis-Neff <damiendavisneff@gmail.com>
2023-07-26 17:36:33 -0400

    Update firebase_deploy.bat

--- firebase_deploy.bat
+++ firebase_deploy.bat
@@ -6,6 +6,7 @@ set /p webhookTitle=<webhook_title.txt
 set /p webhookDesc=<webhook_description.txt
 
 curl -i -H "Accept: application/json" -H "Content-Type: application/json" -X POST --data "{\"content\": null, \"embeds\": [{\"title\": \"%webhookTitle%\", \"description\": \"%webhookDesc%\",\"color\": 16711680,\"author\": {\"name\": \"%webhookAuthor%\",\"icon_url\": \"%webhookAuthorImg%\"}}],\"attachments\": []}" %webhookUrl%
-pause
 
-::firebase deploy
+firebase deploy
+
+pause
