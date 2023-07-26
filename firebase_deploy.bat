51cfbc6e2cd1929ecfb60e478bb546a12b3fbdf5
Damien Davis-Neff <damiendavisneff@gmail.com>
2023-07-26 17:36:18 -0400

    update file names & made things modular

--- firebase_deploy.bat
+++ firebase_deploy.bat
@@ -1,3 +1,11 @@
-set /p webhookUrl=<.webhook_url.txt
-curl -i -H "Accept: application/json" -H "Content-Type:application/json" -X POST --data "{\"content\": null, \"embeds\": [{\"title\": \"The SirSpiro website has been updated\", \"description\": \"A new version of https://www.sirspiro.com/ has been pushed to firebase, and the rollout has begun.\",\"color\": 16711680,\"author\": {\"name\": \"Damien DavisNeff\",\"icon_url\": \"https://avatars.githubusercontent.com/u/105996288?v=4\"}}],\"attachments\": []}" %webhookUrl%
-firebase deploy
+@echo off
+set /p webhookUrl=<webhook_url.txt
+set /p webhookAuthor=<webhook_author.txt
+set /p webhookAuthorImg=<webhook_image.txt
+set /p webhookTitle=<webhook_title.txt
+set /p webhookDesc=<webhook_description.txt
+
+curl -i -H "Accept: application/json" -H "Content-Type: application/json" -X POST --data "{\"content\": null, \"embeds\": [{\"title\": \"%webhookTitle%\", \"description\": \"%webhookDesc%\",\"color\": 16711680,\"author\": {\"name\": \"%webhookAuthor%\",\"icon_url\": \"%webhookAuthorImg%\"}}],\"attachments\": []}" %webhookUrl%
+pause
+
+::firebase deploy
