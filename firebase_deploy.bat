f85693c7d80ab4b8979f6c23b4e8c4c8760db53b
Damien Davis-Neff <damiendavisneff@gmail.com>
2023-07-26 22:47:30 -0400

    multiple webhooks allowed

--- firebase_deploy.bat
+++ firebase_deploy.bat
@@ -1,11 +1,16 @@
 @echo off
-set /p webhookUrl=<webhook_url.txt
+:: OLD VARIABLE THAT WAS USED AS ONE LINE
+::set /p webhookUrl=<webhook_url.txt
 set /p webhookAuthor=<webhook_author.txt
 set /p webhookAuthorImg=<webhook_image.txt
 set /p webhookTitle=<webhook_title.txt
 set /p webhookDesc=<webhook_description.txt
 
-curl -i -H "Accept: application/json" -H "Content-Type: application/json" -X POST --data "{\"content\": null, \"embeds\": [{\"title\": \"%webhookTitle%\", \"description\": \"%webhookDesc%\",\"color\": 16711680,\"author\": {\"name\": \"%webhookAuthor%\",\"icon_url\": \"%webhookAuthorImg%\"}}],\"attachments\": []}" %webhookUrl%
+for /F "tokens=*" %%W in (webhook_url.txt) do (
+    curl -i -H "Accept: application/json" -H "Content-Type: application/json" -X POST --data "{\"content\": null, \"embeds\": [{\"title\": \"%webhookTitle%\", \"description\": \"%webhookDesc%\",\"color\": 16711680,\"author\": {\"name\": \"%webhookAuthor%\",\"icon_url\": \"%webhookAuthorImg%\"}}],\"attachments\": []}" %%W
+)
+:: OLD CURL COMMAND THAT ONLY SUPPORTS A SINGLE URL
+::curl -i -H "Accept: application/json" -H "Content-Type: application/json" -X POST --data "{\"content\": null, \"embeds\": [{\"title\": \"%webhookTitle%\", \"description\": \"%webhookDesc%\",\"color\": 16711680,\"author\": {\"name\": \"%webhookAuthor%\",\"icon_url\": \"%webhookAuthorImg%\"}}],\"attachments\": []}" %webhookUrl%
 
 firebase deploy
 
